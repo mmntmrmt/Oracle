3f9cc346624ea47e08534850644e119248985d05
Ryo Morimoto <morimoto@M-3.local>
2015-07-01 21:24:06 +0900

    Initial Commit

---
+++ Oracle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Oracle
+//
+//  Created by Ryo Morimoto on 2015/07/01.
+//  Copyright (c) 2015年 Ryo Morimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
